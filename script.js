37359e89fc54e492f995699c3a335feff0dce0a4
Tushar <109269801+tushar0103@users.noreply.github.com>
2023-09-01 11:18:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+{/* <span id="temp" class="fa"></span></h1> */}
+
+
+const tempLoad=()=>{
+    let temp= document.getElementById('temp');
+    temp.innerHTML="&#xf2cb";
+    
+
+    setTimeout(()=>{
+        temp.innerHTML="&#xf2cb";
+        temp.style.color="#f8b627";
+    },1000);
+
+
+    setTimeout(()=>{
+        temp.innerHTML="&#xf2c9";
+    },2000);
+
+    setTimeout(()=>{
+        temp.innerHTML="&#xf2c8";
+    },3000);
+
+    setTimeout(()=>{
+        temp.innerHTML="&#xf2c7";
+        temp.style.color="#d63031";
+    },4000);
+}
+tempLoad();
+
+setInterval(tempLoad,5000);
